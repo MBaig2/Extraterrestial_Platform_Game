f77d70ce792a4f8e211a5717ac650bc1ee50d7e3
Mirza Baig <mirzaabaig11@gmail.com>
2020-07-08 13:35:51 -0700

    Added basic physics

--- sprites.py
+++ sprites.py
@@ -1,32 +1,47 @@
 import pygame as pg
 from settings import *
 
+vec = pg.math.Vector2
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
+        pg.sprite.Sprite.__init__(self)
         self.game = game
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
 
-    def move(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
+        # Vectors
+        self.pos = vec(x, y)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def get_keys(self):
+        self.acc = vec(0, PLAYER_GRAVITY)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+
+        self.acc += self.vel * PLAYER_FRICTION
+        self.vel += self.acc * self.game.dt
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = WIDTH
+        if self.pos.x < 0:
+            self.pos.x = 0
 
     def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
+        self.get_keys()
+        self.rect.midbottom = self.pos
 
 
-class Wall(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites, game.walls
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
